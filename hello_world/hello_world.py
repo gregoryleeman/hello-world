214b38aab6b96672fe36ccc2454c00a60576a32d
Gregory Leeman <gl6@sanger.ac.uk>
2020-03-23 17:24:08 +0000

    added ability to so hello to a lines in a file with 'hello_file' command

--- hello_world/hello_world.py
+++ hello_world/hello_world.py
@@ -1,17 +1,32 @@
-def parse_args():
-	import argparse
-	parser = argparse.ArgumentParser()
-	parser.add_argument("--name", default="world")
-	return parser.parse_args()
-
 def hello(name="world"):
 	import jinja2
 	template = jinja2.Template("Hello {{ name }}!")
-	print(template.render(name=name))
+	return(template.render(name=name))
 
-def main():
-	args = parse_args()
-	hello(args.name)
+def hello_single():
+	import argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument("name", default="world", nargs='?', help="What to say hello to")
+	args = parser.parse_args()
+	print(hello(args.name))
+
+def hello_file():
+	import argparse
+	parser = argparse.ArgumentParser(
+		description = "Say hello to all the lines in a file"
+		)
+	parser.add_argument("file", help="Input file path")
+	parser.add_argument("--out", help="Output file path", default="hello.txt")
+	args = parser.parse_args()
+
+	file_in = open(args.file, "r")
+	file_out = open(args.out, "w")
+	for line in file_in.readlines():
+		file_out.write(hello(line.strip()) + "\n")
+	file_in.close()
+	file_out.close()
+
+	print("Said hello in " + args.out)
 
 if __name__ == "__main__":
-	main()
+	hello_single()
